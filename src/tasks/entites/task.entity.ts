72ee945c11d99a792b9cdb13fe32aa16e677371f
alce65 <alce65@hotmail.es>
2024-04-28 11:12:40 +0200

    Install Cloudinary and add Avatar entity an Prisma Schema

--- src/tasks/entites/task.entity.ts
+++ src/tasks/entites/task.entity.ts
@@ -3,6 +3,6 @@ import { User } from '../../users/entities/user.entity';
 export class Task {
   id: string;
   title: string;
-  owner: Omit<User, 'tasks'>;
+  owner: Omit<User, 'tasks' | 'avatar'>;
   isDone: boolean;
 }
